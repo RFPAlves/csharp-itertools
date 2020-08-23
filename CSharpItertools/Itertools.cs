07347eea8b5ddc9d65104d8efd438e71653f0208
Rafael Alves <rafael.alves@csn.com.br>
2020-08-22 22:49:27 -0300

    Fix indent

--- CSharpItertools/Itertools.cs
+++ CSharpItertools/Itertools.cs
@@ -16,24 +16,24 @@ namespace CSharpItertools
             }
         }
 
-		public IEnumerable<(object, object)> IZipLongest<T1, T2>(IEnumerable<T1> iterable1, IEnumerable<T2> iterable2)
-		{
-			IEnumerator<T1> Enumerator1 = iterable1.GetEnumerator();
-			IEnumerator<T2> Enumerator2 = iterable2.GetEnumerator();
+        public IEnumerable<(object, object)> IZipLongest<T1, T2>(IEnumerable<T1> iterable1, IEnumerable<T2> iterable2)
+        {
+            IEnumerator<T1> Enumerator1 = iterable1.GetEnumerator();
+            IEnumerator<T2> Enumerator2 = iterable2.GetEnumerator();
 
-			while (true)
-			{
-				bool moveNext1 = Enumerator1.MoveNext();
-				bool moveNext2 = Enumerator2.MoveNext();
+            while (true)
+            {
+                bool moveNext1 = Enumerator1.MoveNext();
+                bool moveNext2 = Enumerator2.MoveNext();
 
-				if (!moveNext1 && !moveNext2)
-					break;
+                if (!moveNext1 && !moveNext2)
+                    break;
 
-				object output1 = moveNext1 ? Enumerator1.Current : (object)null;
-				object output2 = moveNext2 ? Enumerator2.Current : (object)null;
+                object output1 = moveNext1 ? Enumerator1.Current : (object)null;
+                object output2 = moveNext2 ? Enumerator2.Current : (object)null;
 
-				yield return (output1, output2);
-			}
-		}
-	}
+                yield return (output1, output2);
+            }
+        }
+    }
 }
